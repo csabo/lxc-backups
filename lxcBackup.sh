f6cefdc89636ab783da3d404e92d97fbf91e5369
Chris Sabo <csabo2@gmail.com>
2016-05-21 18:17:31 -0400

    Update lxcBackup.sh
    -added exclusion list for containers to skip
    -added compress backup option
    -added option to maintain the power state of a container (dont start one that wasn't already running)

--- lxcBackup.sh
+++ lxcBackup.sh
@@ -3,6 +3,10 @@
 # Author: Chris Sabo (https://github.com/csabo)
 # Todo: Logging, and error handling
 
+# options
+compressBackups="true" # if true, backup will be gzipped
+maintainContainerState="true" # if true, the container will not be started after backup if it was not already active (running or frozen)
+
 # directory where gzipped backups should go
 backupDirectory="/mnt/backups"
 
@@ -12,6 +16,12 @@ containerDirectory="/var/lib/lxc"
 # generates an array of each of your containers using an ls (due to logs showing up in there i've added a grep -v)
 containerArray=($(ls "$containerDirectory" |grep -v ".log"))
 
+# array of containers that are currently active
+activeContainers=$(lxc-ls --active)
+
+# array of containers you DONT want stopped or backed up
+excludedContainers=("ignoredContainer")
+
 # cleanly stop the container function
 function stopContainer() {
 	/usr/bin/lxc-stop -n "$1"
@@ -19,17 +29,27 @@ function stopContainer() {
 
 # start the container in the background function
 function startContainer() {
-	/usr/bin/lxc-start -d -n "$1"
+	if [[ "$activeContainers" == *"$container"* ]] && [[ "$maintainContainerState" == "true" ]]; then
+		/usr/bin/lxc-start -d -n "$1"
+	fi
 }
 
 # backup the container function (filename example: ServerName.container.05-20-16.tgz )
 function backupContainer() {
-	/usr/bin/tar --numeric-owner -czf "$backupDirectory"/"$1".container.$(date +%m-%d-%y).tgz "$containerDirectory"/"$1"/*
+	if [[ "$compressBackups" == "true" ]]; then
+		/usr/bin/tar --numeric-owner -czf "$backupDirectory"/"$1".container.$(date +%m-%d-%y).tgz "$containerDirectory"/"$1"/*
+	else
+		/usr/bin/tar --numeric-owner -cf "$backupDirectory"/"$1".container.$(date +%m-%d-%y).tar "$containerDirectory"/"$1"/*
+	fi
 }
 
 # iterate through the array of your containers, and back them up
 for container in "${containerArray[@]}"; do
-	stopContainer "$container"
-	backupContainer "$container"
-	startContainer "$container"
+	for ignoredContainer in "${excludedContainers[@]}"; do
+		if [[ ! "$ignoredContainer" == "$container" ]]; then
+			stopContainer "$container"
+			backupContainer "$container"
+			startContainer "$container"
+		fi
+	done
 done
